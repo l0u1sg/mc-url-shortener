6355facf3b31cb04f7d17e43dda4a66366f029a7
Louis Gallet <me@louisgallet.fr>
2025-03-31 16:30:00 -0400

    feat(front-end): :sparkles:  Change homepage to adapt it for URL shortener
    Change the form and the import

--- front-end/src/HomePage.tsx
+++ front-end/src/HomePage.tsx
@@ -1,67 +1,85 @@
 import React, { FormEvent, useState } from 'react';
-import logo from './logo.svg';
-import {
-  useAddMessageMutation,
-  useGetMessagesQuery,
-} from './graphql/message.generated';
+import { useAddUrlMutation, useGetUrlsQuery } from './graphql/url_shortener.generated';
 
 function HomePage() {
-  const [newMessage, setNewMessage] = useState({ value: '' });
-  const { data, refetch } = useGetMessagesQuery();
-  const [addMessage] = useAddMessageMutation();
+  const [newUrl, setNewUrl] = useState({ originalUrl: '', shorten: '' });
+  const { data, refetch } = useGetUrlsQuery();
+  const [addUrl] = useAddUrlMutation();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewMessage({ value: e.target.value });
+    const { name, value } = e.target;
+    setNewUrl((prevUrl) => ({ ...prevUrl, [name]: value }));
   };
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (newMessage.value) {
-      await addMessage({ variables: { message: newMessage.value } });
-      setNewMessage({ value: '' });
-      await refetch();
+    if (newUrl.originalUrl && newUrl.shorten) {
+      const urlExists = data?.urlShorteners.some((url) => url.shortUrl === newUrl.shorten);
+      if (!urlExists) {
+        await addUrl({
+          variables: {
+            originalUrl: newUrl.originalUrl,
+            shorten: newUrl.shorten,
+          },
+        });
+        setNewUrl(newUrl);
+        await refetch();
+      } else {
+        alert('This short URL already exists. Please choose another one.');
+      }
     }
   };
 
   return (
-    <div className="bg-main-blue min-h-screen">
-      <section className="container mx-auto py-8">
+    <section className="container mx-auto py-8">
+      <div
+        data-cy="urlContainer"
+        className="p-8 flex flex-col gap-6 items-center bg-white rounded-2xl"
+      >
         <div
-          data-cy="messageContainer"
-          className="p-8 flex flex-col gap-6 items-center bg-white rounded-2xl"
+          className="font-semibold text-xl"
         >
-          <div
-            className="font-semibold text-xl"
-          >
-            Add a few messages to ensure that everything is working correctly :
-          </div>
-          {data?.messages.map((message) => (
-            <div key={message.id}>{message.message}</div>
-          ))}
-          <div className="font-semibold">
-            <form
-              className="flex gap-4"
-              onSubmit={onSubmit}
-            >
-              <input
-                data-cy="messageInput"
-                placeholder="Your message"
-                className="p-3 w-96 border-2 rounded-full border-main-blue"
-                value={newMessage.value}
-                onChange={onChange}
-              />
-              <button
-                data-cy="submit"
-                type="submit"
-                className="p-3 bg-main-blue text-white rounded-full"
-              >
-                Add message
-              </button>
-            </form>
-          </div>
+          Ajouter une URL à raccourcir :
         </div>
-      </section>
-    </div>
+        {data?.urlShorteners.map((url) => (
+          <div key={url.id}>
+            <a href={`/${url.shortUrl}`} target="_blank" rel="noreferrer noopener">
+              {url.originalUrl}
+            </a>
+          </div>
+        ))}
+        <div className="font-semibold">
+          <form
+            className="flex gap-4"
+            onSubmit={onSubmit}
+          >
+            <input
+              data-cy="originalUrlInput"
+              name="originalUrl"
+              placeholder="https://mobile.club"
+              className="p-3 w-96 border-2 rounded-full border-main-blue"
+              value={newUrl.originalUrl}
+              onChange={onChange}
+            />
+            <input
+              data-cy="shortenUrlInput"
+              name="shorten"
+              placeholder="mobileclub"
+              className="p-3 w-96 border-2 rounded-full border-main-blue"
+              value={newUrl.shorten}
+              onChange={onChange}
+            />
+            <button
+              data-cy="submit"
+              type="submit"
+              className="p-3 bg-main-blue text-white rounded-full"
+            >
+              Raccourcir
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
 
